ae8c38b72aa55878ac579d604332f89f3f5b1405
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-01-01 17:29:16 +0100

    chore: new test

---
+++ packages/groqz/src/__tests__/__examples__/arrays.ts
@@ -0,0 +1,24 @@
+import { groq } from 'groqz'
+
+const query1 = groq`*[_type in ["category", "page"]]{..., category->{title}}`
+const query2 = groq`*[_type == "category"]{title, _type, "image": image.asset->{url, size}}[0]`
+const query3 = groq`*[_type == "movie"]`
+
+console.log(
+  query1.schema
+    .parse([
+      { _type: 'category', title: 'Category' },
+      { _type: 'page', title: 'Page' },
+    ])
+    .map((item) => item.title?.toUpperCase())
+)
+
+console.log(
+  query2.schema.parse({
+    _type: 'category',
+    title: 'Category',
+    image: { url: 'https://example.com/image.png', size: 1024 },
+  }).image?.url
+)
+
+console.log(query3.schema.parse([{ _type: 'movie' }])[0])
